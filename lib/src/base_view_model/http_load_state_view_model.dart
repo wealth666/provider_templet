5e055bf228b5b7a450197bc98eca4b9676b2220d
seven <8536397+seven001@user.noreply.gitee.com>
2021-10-12 21:29:35 +0800

    feat:更新模板

---
+++ lib/src/base_view_model/http_load_state_view_model.dart
@@ -0,0 +1,10 @@
+import '../../provider_templet.dart';
+import 'common_view_model.dart';
+
+abstract class HttpLoadStateViewModel<R> extends CommonViewModel {
+  // 加载数据
+  Future<ResultData<R>> load({bool defaultSet, bool setState});
+
+  // 刷新数据
+  Future<ResultData<R>> refresh();
+}
